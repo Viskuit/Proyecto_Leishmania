70420e4e678e29b0ca7c386a7e8c1fbc0e38f0a2
viskuit <ronnyfph@gmail.com>
2024-03-27 10:20:27 +0100

    Launched 20 times

--- SIDER_RepetitiveSearcher/bedops_test_2_whole_genome.py
+++ SIDER_RepetitiveSearcher/bedops_test_2_whole_genome.py
@@ -2,11 +2,11 @@ from extra.useful import fasta_extractor
 from modules.blaster import blastn_dic, blastn_blaster, repetitive_blaster
 from modules.files_manager import folder_creator
 
-repetitive_blaster("../SIDER_Data/Z_BEDOPS_Test2_whole_genome/TriTrypDB-67_LinfantumJPCM5_Genome.fasta",
-                   "../SIDER_Data/Z_BEDOPS_Test2_whole_genome/First_Blaster.csv",
-                   "../SIDER_Data/Z_BEDOPS_Test2_whole_genome/Results/",
+repetitive_blaster("../genome_data/ZTest2_BEDOPS_laptop/TriTrypDB-67_LinfantumJPCM5_Genome.fasta",
+                   "../genome_data/ZTest2_BEDOPS_laptop/First_Blaster.csv",
+                   "../genome_data/ZTest2_BEDOPS_laptop/Results/",
                    "LinJ",
                    600,
                    1,
-                   2                  
+                   20                  
 )
