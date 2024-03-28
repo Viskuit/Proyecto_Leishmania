78aa01f236b2277e95068463d0b885d862467d75
rfpacheco <ronnyfph@gmail.com>
2024-03-28 10:38:51 +0100

    Important changes in seq_modifier.ppy

--- SIDER_RepetitiveSearcher/Test_whole_genome_with_chr32.py
+++ SIDER_RepetitiveSearcher/Test_whole_genome_with_chr32.py
@@ -1,12 +1,15 @@
 from extra.useful import fasta_extractor
 from modules.blaster import blastn_dic, blastn_blaster, repetitive_blaster
 from modules.files_manager import folder_creator
+import os
+# print(os.getcwd())
+# os.chdir("./SIDER_RepetitiveSearcher")
 
-repetitive_blaster("../genome_data/ZTest2_BEDOPS_laptop/TriTrypDB-67_LinfantumJPCM5_Genome.fasta",
-                   "../genome_data/ZTest2_BEDOPS_laptop/First_Blaster.csv",
-                   "../genome_data/ZTest2_BEDOPS_laptop/Results/",
+repetitive_blaster("../../genome_data/ZTest3_laptop_with_chr32/TriTrypDB-67_LinfantumJPCM5_Genome.fasta",
+                   "../../genome_data/ZTest3_laptop_with_chr32/First_Blaster.csv",
+                   "../../genome_data/ZTest3_laptop_with_chr32/Results/",
                    "LinJ",
                    600,
-                   1,
+                   2,
                    20                  
 )
