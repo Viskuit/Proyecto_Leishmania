cf1de064002feb912fe96b4ef5ccd1dca81e61b2
viskuit <ronnyfph@gmail.com>
2024-05-29 14:43:57 +0200

    Still in stop method

--- SIDER_RepetitiveSearcher/modules/stopping.py
+++ SIDER_RepetitiveSearcher/modules/stopping.py
@@ -46,38 +46,15 @@ def stopping_bedops(new_data, old_data, folder_path, genome_fasta):
 
     # -----------------------------------------------------------------------------
     ## Call BEDOPS on plus
-    coincidence_plus, data_plus = bedops_stopping(new_data_plus, old_data_plus, plus_path, "plus", genome_fasta)
+    result_plus = bedops_stopping(new_data_plus, old_data_plus, plus_path, query_cover=90)
 
+    # -----------------------------------------------------------------------------
     # Call BEDOPS on minus. Special case, because BEDOPS reads the coordinates like the "+" strand.
     ## First modify the coordinates.
     new_data_minus[["sstart", "send"]] = new_data_minus[["send", "sstart"]]
     old_data_minus[["sstart", "send"]] = old_data_minus[["send", "sstart"]]
 
+    ## And now call BEDOPS on minus
+    result_minus = bedops_coincidence(new_data_minus, old_data_minus, minus_path)
     # -----------------------------------------------------------------------------
-## And now call BEDOPS on minus
-    coincidence_minus, data_minus = bedops_coincidence(new_data_minus, old_data_minus, minus_path. "minus", genome_fasta)
-    if not data_minus.empty:  # If the data frame is not empty
-        data_minus[["sstart", "send"]] = data_minus[["send", "sstart"]]  # restore "data_minus" coordinates
-
-    recapture_data = pd.concat([data_plus, data_minus], ignore_index=True)  # joins both Data Frames
-    if not recapture_data.empty:  # If the data frame is not empty
-        recapture_data = recapture_data.sort_values(by=["sstrand", "sseqid", "sstart"])  # Sort the data frame by the start coordinate
- 
-    # -----------------------------------------------------------------------------
-    ## math part
-    total_coincidence = coincidence_plus + coincidence_minus
-    perc_coincidence = total_coincidence / old_data.shape[0] * 100
-    print(f"\t\t\t- Coincidence (BEDOPS version):\n",
-          f"\t\t\t\t- {total_coincidence}/{old_data.shape[0]} - {perc_coincidence:.2f}%")
     
-    # -----------------------------------------------------------------------------
-    ## Now recapture data
-
-    
-    if total_coincidence == old_data.shape[0]:
-        print(f"\t\t\t\t- TRUE")
-        return True, recapture_data
-    else:  # If the the coincidence is not the 100%
-        print(f"\t\t\t\t- FALSE")
-        return False, recapture_data
-
